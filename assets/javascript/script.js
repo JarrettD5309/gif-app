a00bb1b63ba28e4631910134ddecbf55bbda1f0c
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-05-13 00:43:36 -0400

    did css layout, fixed some bugs in JS including queryUrl var

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,5 @@
-var topics = ["New York Yankees","Boston Red Sox","Philadelphia Phillies","Baltimore Orioles","Tampa Bay Rays"];
+var topics = ["New York Yankees","Boston Red Sox","Chicago Cubs","Baltimore Orioles","Tampa Bay Rays"];
+
 
 var buttonDiv = $("#button-div");
 var imageDiv = $("#image-div");
@@ -6,23 +7,44 @@ var team = "";
 
 var apiKey = "SF7TXcXQN2SXAfb2w6nQGXK8QO6autqa";
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=10" + "&q=";
+
 
 function addButtons() {
+    var divCounter = 1;
     buttonDiv.empty();
     for (var i=0;i<topics.length;i++) {
         var newButton = $("<button>");
         newButton.text(topics[i]);
         newButton.addClass("gif-button");
         newButton.attr("data-name", topics[i]);
+
+        if (divCounter===1) {
+            newButton.addClass("col-4");
+            divCounter++;
+        } else if (divCounter===2) {
+            newButton.addClass("col-4 no-left-border");
+            divCounter++;
+        } else if (divCounter===3) {
+            newButton.addClass("col-4 no-left-border");
+            divCounter++;
+        } else if (divCounter===4) {
+            newButton.addClass("col-6");
+            divCounter++;
+        } else if (divCounter===5) {
+            newButton.addClass("col-6 no-left-border");
+            divCounter=1;
+        }
+
         buttonDiv.append(newButton);
     }
 };
 
 $(document).on("click",".gif-button", function(){
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=10&q=";
     team = $(this).attr("data-name");
     console.log(team);
     queryURL = queryURL + team;
+    console.log(queryURL);
     $.ajax(
         {url: queryURL,
         method: "GET"}
@@ -31,7 +53,7 @@ $(document).on("click",".gif-button", function(){
                 var individualImageDiv = $("<div>");
                 individualImageDiv.addClass("indiv-img-div");
 
-                individualImageDiv.append($("<img>").attr("src", response.data[i].images.fixed_height_still.url).attr("data-still",response.data[i].images.fixed_height_still.url).attr("data-animate",response.data[i].images.fixed_height.url).attr("data-state", "still").addClass("gif"));
+                individualImageDiv.append($("<img>").attr("src", response.data[i].images.fixed_width_still.url).attr("data-still",response.data[i].images.fixed_width_still.url).attr("data-animate",response.data[i].images.fixed_width.url).attr("data-state", "still").addClass("gif"));
 
                 individualImageDiv.append($("<p>").text("Rating: " +response.data[i].rating).addClass("rating"));
                 imageDiv.prepend(individualImageDiv);
@@ -52,10 +74,15 @@ $(document).on("click",".gif", function() {
 
 $("#add-gif").on("click", function() {
     event.preventDefault();
+    if ($("#gif-input").val()==="") {
+
+    } else {
+    
     var newTopic = $("#gif-input").val().trim();
     topics.push(newTopic);
     $("#gif-input").val("");
     addButtons();
+    }
 })
 
 addButtons();
